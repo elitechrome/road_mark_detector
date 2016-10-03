d46300924a2c8e32f1235ff59ac34a988bc8dfb7
jaemin <ljm6525@gmail.com>
2016-10-04 01:14:50 +0900

    Implemented IPM, Thresholding, Color Picking roughly.

--- src/road_mark_detector_node.cpp
+++ src/road_mark_detector_node.cpp
@@ -12,6 +12,7 @@
  * 4.
  *
  */
+using namespace cv;
 class road_mark_detector_node{
 private:
   ros::NodeHandle nh;
@@ -23,66 +24,103 @@ private:
   cv::Mat         K_;
   cv::Mat         D_;
   double          balance_;
+  cv::Mat         img;
 
 public:
   road_mark_detector_node(ros::NodeHandle _nh):nh(_nh), it(_nh){
-    nh.param("topic_name", paramTopicName, std::string("image_raw"));
-    image_sub = it.subscribe("image_raw", 1, &road_mark_detector_node::process, this);
+    nh.param("topic_name", paramTopicName, std::string("/stereo/right/image_raw"));
+    image_sub = it.subscribe(paramTopicName, 1, &road_mark_detector_node::process, this);
 
-    nh.param("camera_info_url", fisheye_camera_info_url_, std::string("~/.ros/camera_info/camera.yml"));
-    nh.param("balance", balance_, 0.2);
-
+    nh.param("camera_info_url", fisheye_camera_info_url_, std::string("/home/jaemin/clothoid_ws/src/road_mark_detector/camera.yml"));
+    nh.param("balance", balance_, 0.6);
+    if(!getCAMINFO()){
+      ROS_ERROR("Couldn't get the camera infomation file on %s", fisheye_camera_info_url_.c_str());
+      return;
+    }
+    ros::spin();
   }
-  void process(const sensor_msgs::ImageConstPtr &img){
-    float width, height;
+
+  void process(const sensor_msgs::ImageConstPtr &msg){
+    try {
+      img = cv_bridge::toCvShare(msg, "bgr8")->image;
+    } catch (cv_bridge::Exception& e) {
+      ROS_ERROR("Fail to convert image.");
+    }
+    cv::Mat temp, undistorted_img_;
+    cv::Matx33d newK = K_;
+    img.copyTo(temp);
+    cv::fisheye::estimateNewCameraMatrixForUndistortRectify(K_, D_, temp.size(), cv::noArray(), newK, balance_);
+    cv::fisheye::undistortImage(temp, undistorted_img_, K_, D_, newK);
+
+    float width = img.cols, height = img.rows;
+
     std::vector<cv::Point2f> origPoints;
-    origPoints.push_back( cv::Point2f(0, height) );
-    origPoints.push_back( cv::Point2f(width, height) );
-    origPoints.push_back( cv::Point2f(width/2+30, 140) );
-    origPoints.push_back( cv::Point2f(width/2-50, 140) );
+    //    origPoints.push_back( cv::Point2f(-120, height) );
+    //    origPoints.push_back( cv::Point2f(width+120, height) );
+    //    origPoints.push_back( cv::Point2f(width/2-5, 235) );
+    //    origPoints.push_back( cv::Point2f(width/2-40, 235) );
+    origPoints.push_back( cv::Point2f(-(3000+200), height) );
+    origPoints.push_back( cv::Point2f(width+3000-200, height) );
+    origPoints.push_back( cv::Point2f(width-(300+15), 235) );
+    origPoints.push_back( cv::Point2f(300-15, 235) );
+
     std::vector<cv::Point2f> dstPoints;
-    dstPoints.push_back( cv::Point2f(0, height) );
-    dstPoints.push_back( cv::Point2f(width, height) );
-    dstPoints.push_back( cv::Point2f(width, 0) );
-    dstPoints.push_back( cv::Point2f(0, 0) );
-    cv::Mat inputImg, outputImg;
-    // IPM object
+    double scale = 2., ratio = 4;
+    dstPoints.push_back( cv::Point2f(width/2-width/2*scale, height) );
+    dstPoints.push_back( cv::Point2f(width/2+width/2*scale, height) );
+    dstPoints.push_back( cv::Point2f(width/2+width/2*scale, height/2 - height/2*scale*ratio) );
+    dstPoints.push_back( cv::Point2f(width/2-width/2*scale, height/2 - height/2*scale*ratio) );
+
+
+    cv::Mat inputThresholdImg, inputThresholdImg_ROI, outputIPMImg, outputThresholdImg;
+    cv::cvtColor(undistorted_img_, inputThresholdImg, CV_BGR2GRAY);
+    inputThresholdImg_ROI = inputThresholdImg(cv::Rect(0, inputThresholdImg.rows/2, inputThresholdImg.cols, inputThresholdImg.rows/2));
+
+    //equalizeHist( inputThresholdImg_ROI, inputThresholdImg_ROI );
+
+    //Todo : Laplacian(Emboss filter)
+    cv::threshold(inputThresholdImg, outputThresholdImg, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+
+    cv::Mat hsv_image;
+    cv::cvtColor(undistorted_img_, hsv_image, cv::COLOR_BGR2HSV);
+
+    cv::Mat HSV_yellow_range;
+    cv::inRange(hsv_image, cv::Scalar(10, 70, 0), cv::Scalar(35, 255, 255), HSV_yellow_range);
+
     IPM ipm( cv::Size(width, height), cv::Size(width, height), origPoints, dstPoints );
-     ipm.applyHomography( inputImg, outputImg );
+    ipm.applyHomography( outputThresholdImg, outputIPMImg );
+    //ipm.drawPoints(origPoints, undistorted_img_ );
+
+    cv::imshow("original", img);
+    cv::imshow("undistort", undistorted_img_);
+    cv::imshow("IPM", outputIPMImg);
+    cv::imshow("Threshold", outputThresholdImg);
+    cv::imshow("HSV_yellow_range", HSV_yellow_range);
+
+    cv::waitKey(1);
   }
+
   bool getCAMINFO() {
+    cv::FileStorage fs;
 
-      cv::FileStorage fs;
-      cv::Matx33d     K;
-      cv::Mat         D;
-
-      if (fs.open(fisheye_camera_info_url_, cv::FileStorage::READ) ) {
-
-          fs["camera_matrix"] >> K_;
-          fs["distortion_coefficients"] >> D_;
-
-          fs.release();
-
-          return true;
-      } else {
-          return false;
-      }
-
+    if (fs.open(fisheye_camera_info_url_, cv::FileStorage::READ) ) {
+      fs["camera_matrix"] >> K_;
+      fs["distortion_coefficients"] >> D_;
+      fs.release();
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  bool calib_fisheye() {
-    cv::Mat temp, process_img_;
-          cv::Matx33d newK = K_;
 
-          cv::fisheye::estimateNewCameraMatrixForUndistortRectify(K_, D_, temp.size(), cv::noArray(), newK, balance_);
-          cv::fisheye::undistortImage(temp, process_img_, K_, D_, newK);
-
-  }
 };
- int main (int argc, char** argv)
- {
-   ros::init(argc, argv, "road_mark_detector_node");
-   ros::NodeHandle nh;
-   road_mark_detector_node node(nh);
-   return 0;
- }
+int main (int argc, char** argv)
+{
+  ros::init(argc, argv, "road_mark_detector_node");
+  ros::NodeHandle nh;
+  ROS_INFO("road_mark_detector_node has started.");
+  road_mark_detector_node node(nh);
+  return 0;
+}
